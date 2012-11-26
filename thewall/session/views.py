7e5e0877232601a83c5352135b77ba55b944a47e
Nick Catalano <nickcatal@gmail.com>
2012-11-26 13:03:08 -0500

    Changing the way lists are populated

--- thewall/session/views.py
+++ thewall/session/views.py
@@ -1,13 +1,20 @@
 from django.http import Http404
-from django.shortcuts import redirect
+from django.shortcuts import redirect, render_to_response
 
 from thewall.session.models import Session, Room, Venue, Slot, Day, SessionTag
 
 from thewall.utility.decorators import render_to
 
-@render_to("home.html")
+import datetime
+
+@render_to()
 def home(request):
-    return { 'sessions': Session.objects.select_related().all() }
+    return { 
+        'currentsessions': Session.objects.select_related().filter(slot__day__day__gte=datetime.datetime.today).order_by('slot__day','slot__start_time'),
+        'pastsessions': Session.objects.select_related().filter(slot__day__day__lt=datetime.datetime.today).order_by('slot__day','slot__start_time'),
+        'template': 'list.html'
+    }
+
 
 def filter(request):
     if request.method == 'GET':
