48288f482390135e3f7fe2020a6e40b3aac4641c
Nick Catalano <nickcatal@gmail.com>
2012-11-25 19:15:25 -0500

    Adding 404 and 500 templates

--- thewall/settings.py
+++ thewall/settings.py
@@ -102,11 +102,7 @@ ROOT_URLCONF = 'thewall.urls'
 # Python dotted path to the WSGI application used by Django's runserver.
 WSGI_APPLICATION = 'thewall.wsgi.application'
 
-TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-)
+TEMPLATE_DIRS = [APP.child('templates')]
 
 INSTALLED_APPS = (
     'django.contrib.auth',
