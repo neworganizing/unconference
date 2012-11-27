4b1c46eb30003710d4b400488a3f05a775421432
Nick Catalano <nickcatal@gmail.com>
2012-11-26 22:23:46 -0500

    Fixing a possible security issue with the API plugin

--- thewall/api/urls.py
+++ thewall/api/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 from djangorestframework.resources import ModelResource
-from djangorestframework.views import ListOrCreateModelView, InstanceModelView
+from djangorestframework.views import ListModelView, InstanceModelView
 from thewall.session.models import Session
 
 class MyResource(ModelResource):
@@ -20,6 +20,6 @@ class MyResource(ModelResource):
 
 urlpatterns = patterns('',
 	url(r'^restframework', include('djangorestframework.urls', namespace='djangorestframework')),
-	url(r'^$', ListOrCreateModelView.as_view(resource=MyResource)),
+	url(r'^$', ListModelView.as_view(resource=MyResource)),
 	url(r'^(?P<pk>[^/]+)/$', InstanceModelView.as_view(resource=MyResource)),
 )
