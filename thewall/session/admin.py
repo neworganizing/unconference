aafa7f5116aae7086253f4923d978e636e341c13
Tim Anderegg <tim@neworganizing.com>
2012-11-29 10:25:53 -0500

    Increased size of tags multiselect

--- thewall/session/admin.py
+++ thewall/session/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.forms import SelectMultiple
 
 from ajax_select import make_ajax_form
 from ajax_select.admin import AjaxSelectAdmin
@@ -8,5 +9,6 @@ from thewall.participants.models import Participant
 
 class SessionAdmin(AjaxSelectAdmin):
 	form = make_ajax_form(Session, {'presenters': 'participant'})
+	formfield_overrides = { models.ManyToManyField: {'widget': SelectMultiple(attrs={'size':'15'})}, }
 
 admin.site.register(Session, SessionAdmin)
