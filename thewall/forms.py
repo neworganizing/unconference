2282f0de5f97c8e996bc0376ecd997c60479fb9a
Tim Anderegg <tim@neworganizing.com>
2014-04-18 13:28:28 -0400

    Cleaned up new session form a bit

--- thewall/forms.py
+++ thewall/forms.py
@@ -2,5 +2,8 @@ from django import forms
 from thewall.models import Session
 
 class NewSessionForm(forms.ModelForm):
+
 	class Meta:
 		model = Session
+		fields = ('title', 'description', 'headline', 'presenters', 'tags', 'difficulty')
+		widgets = {"title": forms.TextInput(), 'headline': forms.TextInput()}
